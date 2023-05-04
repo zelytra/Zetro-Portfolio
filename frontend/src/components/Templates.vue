443066e444a51a1f954b21a114b495dd11f0bb0d
PETIT Alexandre <alexbreuillet@gmail.com>
2023-05-04 15:58:31 +0200

    Add Templates page

--- frontend/src/components/Templates.vue
+++ frontend/src/components/Templates.vue
@@ -1,11 +1,130 @@
 <template>
-  <p>Template</p>
+  <section class="projects">
+    <Loading v-if="loading"/>
+    <Title v-if="templateProvider" :title="templateProvider.title" :sub-title="templateProvider.subTitle" class="title"/>
+    <div class="filters-wrapper">
+      <SelectInput v-if="templateProvider"
+                   v-for="provider of templateProvider.filters"
+                   :options="provider.tags"
+                   @select-change="updateFilter(provider.name,$event)"
+                   :place-holder="provider.name"/>
+    </div>
+    <div class="card-wrapper" v-if="!loading">
+      <transition-group>
+        <TemplateCard :template="template"
+                      v-for="template of filteredTemplate.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())"
+                      @click="redirectTo(template.redirection)"
+                      :key="template.name"/>
+      </transition-group>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
+import {onMounted, ref} from "vue";
+import {GitNode} from "@/object/GitNode";
+import {HTTPAxios} from "@/object/HTTPAxios";
+import Title from "@/vue/global/Title.vue";
+import Loading from "@/vue/global/Loading.vue";
+import SelectInput from "@/vue/global/SelectInput.vue";
+import {langStore} from "@/store/LangStore";
+import {Template, TemplateProvider} from "@/object/Template";
+import TemplateCard from "@/vue/template/TemplateCard.vue";
 
+const templates = ref<Template[]>([])
+const filterMap = ref<Map<string, string[]>>(new Map<string, string[]>())
+const filteredTemplate = ref<Template[]>([])
+const nodes = ref<GitNode[]>([])
+const loading = ref(false)
+
+const templateProvider = ref<TemplateProvider>()
+
+onMounted(() => {
+  loading.value = true
+  new HTTPAxios("user/templates.json", null, true).get().then((data) => {
+    templateProvider.value = data.data as TemplateProvider
+  })
+  new HTTPAxios("git/template/" + langStore.get(), null, false).get().then(async (response) => {
+    nodes.value = response.data
+    for (const node of nodes.value) {
+      await loadTemplate(node.url)
+    }
+    updateFilter('', [])
+    loading.value = false
+  })
+})
+
+function updateFilter(key: string, value: string[]) {
+  filterMap.value.set(key, value);
+
+  filteredTemplate.value = templates.value;
+  if (filterMap.value.size > 0) {
+    // filter projects that do not match the filters
+    const filterValues = templates.value.filter((template) => {
+      return Array.from(filterMap.value).some(([filterKey, filterValues]) => {
+        return template.tags.some((tagValue) => filterValues.includes(tagValue));
+      });
+    });
+
+    if (Array.from(filterMap.value.values()).every((value) => value.length === 0)) {
+      // no filter, show all projects
+      filteredTemplate.value = templates.value;
+    } else {
+      // filter projects that do not match the filters
+      filteredTemplate.value = filterValues;
+    }
+  }
+}
+
+async function loadTemplate(url: string) {
+  await new HTTPAxios(url.split(langStore.get() + "/")[1], null, true).get().then((response) => {
+    templates.value.push(response.data)
+  })
+}
+
+function redirectTo(link: string) {
+  window.open(link, "blank")
+}
 </script>
 
 <style scoped lang="scss">
+section.projects {
+  padding: 60px 10%;
+  max-width: 1600px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  gap: 60px;
+  align-items: center;
+  position: relative;
+  overflow: hidden;
+  min-height: 400px;
 
+  .filters-wrapper {
+    display: flex;
+    gap: 12px;
+    align-items: center;
+    align-self: start;
+  }
+
+  .card-wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 18px;
+    width: 100%;
+  }
+}
+
+.v-enter-active {
+  transition: 0.2s ease;
+}
+
+.v-leave-active {
+  position: absolute;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
 </style>
---
+++ frontend/src/vue/template/TemplateCard.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="template-card">
+    <div class="content banner" :style="{backgroundImage:'url('+props.template.banner+')'}"/>
+    <div class="content details">
+      <div class="title">
+        <h4>{{ props.template.name }}</h4>
+        <p>{{ props.template.price }}€</p>
+      </div>
+      <div class="footer-details">
+        <div class="tags">
+          <span v-for="tag of props.template.tags">{{ tag }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {PropType} from "vue";
+import {Template} from "@/object/Template";
+
+const props = defineProps({
+  template: {
+    type: Object as PropType<Template>,
+    required: true
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.template-card {
+  width: 285px;
+  height: fit-content;
+  cursor: pointer;
+  flex-shrink: 0;
+
+  &:hover {
+    background: rgba(255, 232, 209, 0.1);
+    transform: translateY(-8px);
+    border-radius: 8px;
+
+    .content.banner {
+      opacity: 1;
+    }
+  }
+
+  .content {
+    border: solid 1px var(--main);
+    border-top: none;
+    border-bottom-left-radius: 8px;
+    border-bottom-right-radius: 8px;
+
+    &.banner {
+      width: 285px;
+      height: 330px;
+      aspect-ratio: auto;
+      border: none;
+      background-position: center;
+      background-size: cover;
+      background-repeat: no-repeat;
+      border-radius: 8px 8px 0 0;
+      background-color: var(--secondary-background);
+      opacity: 50%;
+    }
+
+    &.details {
+      padding: 18px;
+      display: flex;
+      flex-direction: column;
+      gap: 22px;
+      min-height: 60px;
+
+      .footer-details {
+        display: flex;
+        justify-content: space-between;
+        gap: 8px;
+        align-items: start;
+
+        img {
+          padding-top: 8px;
+          width: 20px;
+        }
+      }
+
+      .title {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        h4 {
+          font-family: PlusJakartaSans, sans-serif;
+          font-style: normal;
+          font-weight: 500;
+          font-size: 16px;
+          line-height: 20px;
+          color: var(--main);
+        }
+      }
+
+      .tags {
+        display: flex;
+        flex-wrap: wrap;
+        //overflow: hidden;
+        gap: 6px;
+
+        span {
+          padding: 5px 8px;
+          border: 1px solid #FFE8D1;
+          border-radius: 4px;
+          font-style: normal;
+          font-weight: 500;
+          font-size: 12px;
+          line-height: 15px;
+          color: var(--main);
+        }
+      }
+    }
+  }
+}
+</style>
